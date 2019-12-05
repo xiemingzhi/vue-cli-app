f6c23247a0a8430f68dbca86252c2b1158f0a93f
ming <xiemingzhi@gmail.com>
2019-12-05 16:08:04 +0800

    Add bootstrap-vue example

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <b-alert show variant="danger" dismissible>
+      Dismissible Alert!
+    </b-alert>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
